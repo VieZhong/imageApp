102b4f54bd53f1b03f7ef83daf73048dcdfd96a1
VieZhong <zyw13579@qq.com>
2017-04-12 15:32:27 +0800

    bring webpack and add dependencies

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            include: [path.resolve(__dirname, "src")],
+            enforce: "post",
+            loader: "babel-loader",
+            options: {
+                presets: ["es2015"]
+            }
+        }, {
+            test: /\.scss$/,
+            use: ["style-loader", "css-loader", "sass-loader"]
+        }]
+    },
+
+    resolve: {
+        modules: [
+            "node_modules",
+            path.resolve(__dirname, "src")
+        ],
+        extensions: [".js", ".jsx", ".json"]
+    },
+
+    devtool: "eval-source-map",
+
+    context: __dirname,
+
+    devServer: {
+        compress: true, // enable gzip compression
+        historyApiFallback: true, // true for index.html upon 404, object for multiple paths
+        hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+    },
+
+    plugins: [
+        new HtmlwebpackPlugin({
+            filename: 'index.html',
+            title: 'ImageApp'
+        })
+    ]
+};
